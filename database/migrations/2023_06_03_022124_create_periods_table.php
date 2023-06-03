810df70e3be4229b6afbcb67055ba204a294d4ff
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-03 10:54:45 +0700

    feat: add periods and schedules tables migration

---
+++ database/migrations/2023_06_03_022124_create_periods_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('periods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('Nama Periode');
+            $table->date('start_date')->comment('Tanggal Mulai');
+            $table->date('end_date')->comment('Tanggal Berakhir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('periods');
+    }
+};
