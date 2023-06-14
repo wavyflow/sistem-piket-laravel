87fc084c5910b7b6bc6e4be6ef84d6db5de27678
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-14 10:01:15 +0700

    feat: add Presensi page with presensi action

---
+++ app/Filament/Pages/Presence.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\Presence as ModelsPresence;
+use Filament\Pages\Page;
+use Filament\Pages\Actions\Action;
+use Filament\Forms;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+class Presence extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-key';
+
+    protected static string $view = 'filament.pages.presence';
+
+    protected static ?string $navigationLabel = 'Presensi';
+
+    protected ?string $heading = 'Presensi';
+
+    protected function getActions(): array
+    {
+        return [
+            Action::make('presensi')
+                ->hidden(function (): bool {
+                    return ModelsPresence::where('user_id', Auth::id())
+                        ->whereDate('created_at', Carbon::today())->count() > 0;
+                })
+                ->label('Presensi')
+                ->action(function (array $data): void {
+                    $data['user_id'] = Auth::id();
+
+                    ModelsPresence::create($data);
+
+                    redirect('/presence');
+                })->form([
+                    Forms\Components\Textarea::make('keterangan')
+                    ->label('Keterangan')
+                    ->required(),
+                ]),
+        ];
+    }
+}
