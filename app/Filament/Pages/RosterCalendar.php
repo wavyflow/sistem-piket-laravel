140271094849138cfb45410d34be3942cdb3528c
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-16 16:08:31 +0700

    feat: add new feature to RosterCalendar page

--- app/Filament/Pages/RosterCalendar.php
+++ app/Filament/Pages/RosterCalendar.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Pages;
 
 use App\Filament\Widgets\CalendarWidget;
+use App\Models\Schedule;
+use App\Models\Squad;
+use Carbon\CarbonImmutable;
 use Filament\Pages\Page;
+use Termwind\Components\Dd;
 
 class RosterCalendar extends Page
 {
@@ -13,10 +17,55 @@ class RosterCalendar extends Page
 
     protected static ?string $title = 'Kalender Piket';
 
+    protected static ?int $navigationSort = 5;
+
+
     protected function getHeaderWidgets(): array
     {
         return [
-            CalendarWidget::class
+            // CalendarWidget::class
+        ];
+    }
+
+    protected function getViewData(): array
+    {
+        $today = CarbonImmutable::today();
+        $squads = Squad::with([
+            'schedules',
+            'schedules.period'
+            ])->whereHas('schedules', function ($q) use ($today) {
+                $q->where('week', $today->weekOfMonth);
+            })->get();
+
+        $result = [];
+
+        // Menambahkan array kosong apabila
+        foreach ($squads as $i => $squad) {
+            $result[$i] = $squad->toArray();
+            $newSchedules = [];
+            for ($j = 1; $j <= 7; $j++) {
+                $add = true;
+                foreach ($result[$i]['schedules'] as $schedule) {
+                    if ($schedule['day'] === $j) {
+                        $newSchedules[] = $schedule;
+                        $add = false;
+                    }
+                }
+
+                if ($add) {
+                    $newSchedules[] = [];
+                }
+            }
+            $result[$i]['schedules'] = $newSchedules;
+        }
+
+        return [
+            'squads' => $result,
+            'schedules' => $result,
+            'today' => $today,
+            'days' => [
+                'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'
+            ]
         ];
     }
 }
