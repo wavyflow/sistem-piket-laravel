8150a522d94489938336c453de78632c8bfb3041
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-18 09:56:28 +0700

    style: Remove unused import and add Tailwind directives

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PresenceReportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/print-presence-report', [PresenceReportController::class, 'index'])->name('print-presence-report');
